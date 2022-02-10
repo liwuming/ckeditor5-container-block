870b1e6b6b1c234f79bbb9e320fccc3313618726
liwuming <1459543371@qq.com>
2022-02-10 15:31:03 +0800

    asa

---
+++ docs/build/translations/bg.js
@@ -0,0 +1,1 @@
+!function(o){const n=o.bg=o.bg||{};n.dictionary=Object.assign(n.dictionary||{},{"%0 of %1":"","Block quote":"Цитат",Bold:"Удебелен",Code:"",Redo:"Повтори","Show more items":"",Underline:"",Undo:"Отмени","Upload in progress":"Качването е в процес"}),n.getPluralForm=function(o){return 1!=o}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
---
+++ docs/build/translations/si.js
@@ -0,0 +1,1 @@
+!function(n){const o=n.si=n.si||{};o.dictionary=Object.assign(o.dictionary||{},{Bold:"තදකුරු","Cannot upload file:":"ගොනුව යාවත්කාලීන කළ නොහැක:",Code:"",Redo:"නැවත කරන්න",Underline:"",Undo:"අහෝසි කරන්න"}),o.getPluralForm=function(n){return 1!=n}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
